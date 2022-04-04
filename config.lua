bd55fd4c3b1d11f79d90b3cc741c3958ea84d83a
Sukrayt <67682343+Sukrayt@users.noreply.github.com>
2022-04-04 18:38:24 +0200

    Fixed Typos

--- config.lua
+++ config.lua
@@ -9,9 +9,9 @@ config.Locale = 'en'    --en or de feel free to add more and PR it on GitHub
 Translation = {         --Add more under en if you want to Translate the Script
     ['en'] = {
       ['wash_money'] = 'Press ~g~E~s~, to wash your Illegal Money',
-      ['wash_succesfull'] = 'You succesfully washed your Illeal Money',
+      ['wash_succesfull'] = 'You succesfully washed your Illegal Money',
       ['no_blackmoney'] = 'You dont have any Illegal Money',
-      ['money_wash_prompt'] = 'Press ~INPUT_CONTEXT~ to wash your ~r~Illgeal Money~s~',
+      ['money_wash_prompt'] = 'Press ~INPUT_CONTEXT~ to wash your ~r~Illegal Money~s~',
     },
     ['de'] = {
       ['wash_money'] = 'Drücke ~g~E~s~, um dein Schwarzgeld zu waschen',
