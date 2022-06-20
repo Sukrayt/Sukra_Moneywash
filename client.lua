649d8b05c8772637bfa74da728541eb118c4f3dd
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2022-06-20 19:25:14 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -14,11 +14,6 @@ end
 --UI
 _menuPool = NativeUI.CreatePool()
 local mainMenu
-local round = function(num, numDecimalPlaces)
-	local mult = 10^(numDecimalPlaces or 0)
-	return math.floor(num * mult + 0.5) / mult
-end
-
 local notify = function(txt)
 	-- CUSTOM NOTIFICATION
 end
